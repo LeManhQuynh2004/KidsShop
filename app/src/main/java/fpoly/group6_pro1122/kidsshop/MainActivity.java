5055c7d128b05ac79f0e46657c8143e8e30931ca
quynhlm.dev <quynhlmph32353@fpt.edu.vn>
2023-11-16 08:05:16 +0700

    Initial commit

---
+++ app/src/main/java/fpoly/group6_pro1122/kidsshop/MainActivity.java
@@ -0,0 +1,14 @@
+package fpoly.group6_pro1122.kidsshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
