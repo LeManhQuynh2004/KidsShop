12a58eb500795fd9ab6bb797492ece66c12d56b6
chinhtdph40493 <chinhtdph40493@fpt.edu.vn>
2023-11-17 14:44:44 +0700

    The Loai

--- app/src/main/java/fpoly/group6_pro1122/kidsshop/Adapter/CategoryAdmin_Adapter.java
+++ app/src/main/java/fpoly/group6_pro1122/kidsshop/Adapter/CategoryAdmin_Adapter.java
@@ -1,16 +1,32 @@
 package fpoly.group6_pro1122.kidsshop.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import fpoly.group6_pro1122.kidsshop.Model.Category;
+import fpoly.group6_pro1122.kidsshop.R;
+
 public class CategoryAdmin_Adapter extends RecyclerView.Adapter<CategoryAdmin_Adapter.CategoryAdmin> {
+    ArrayList<Category> list = new ArrayList<>();
+    Context context;
+
+    public CategoryAdmin_Adapter(ArrayList<Category> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public CategoryAdmin onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_category_admin,parent,false);
+        return new CategoryAdmin(view);
     }
 
     @Override
